ce4c4c94fa19037aa2c79f21634c5ab44b3f081e
Shane Dinh <shane.dinh5@gmail.com>
2022-07-30 22:15:17 -0500

    create login route for authentication

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
 
 // create our User model
 class User extends Model {}
@@ -44,7 +45,23 @@ User.init(
       },
     },
   },
+
   {
+    hooks: {
+      // set up beforeCreate lifecyle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecyle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
